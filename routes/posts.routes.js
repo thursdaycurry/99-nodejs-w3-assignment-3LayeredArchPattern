f77b112c6cf31f172d827bfed455627b8a06760b
thursdaycurry <thursdaycurry@gmail.com>
2023-01-10 01:59:41 +0900

    template setup

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,7 +1,15 @@
+// Router -> Controller
+
 const express = require('express');
 const router = express.Router();
 
 const PostsController = require('../controllers/posts.controller');
+const postsController = new PostsController();
 
 router.get('/', postsController.getPosts); // READ all posts
-router.get('/');
+router.get('/:postId', postsController.getPostById); // READ specific post
+router.post('/', postsController.createPost); // CREATE post
+router.put('/:postId', postsController.updatePost); // UPDATE post
+router.delete('/:postId', postsController.deletePost);
+
+module.exports = router;
