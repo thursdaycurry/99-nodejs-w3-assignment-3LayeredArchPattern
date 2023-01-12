960b4f0fd19202d0d2832ff94781b7b73910c5f6
thursdaycurry <thursdaycurry@gmail.com>
2023-01-12 17:52:11 +0900

    MODIFY moved legacy files

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -6,14 +6,6 @@ const router = express.Router();
 const LoginController = require('../controllers/login.controller');
 const loginController = new LoginController();
 
-// todo - basic core func
-// todo - error handling
-
 router.post('/', loginController.login); // Login
-
-// router.get('/:postId', postsController.getPostById); // READ specific post
-// router.post('/', postsController.createPost); // CREATE post
-// router.put('/:postId', postsController.updatePost); // UPDATE post
-// router.delete('/:postId', postsController.deletePost); // DELETE post
 
 module.exports = router;
